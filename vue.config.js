b8a20f6143ea1381504c8ed3d28e2edc5d0cba7f
Johnny <jarena3@users.noreply.github.com>
2021-11-05 11:17:26 -0500

    suppress vue-cli cache behavior

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
+  chainWebpack: config => {
+    config.module.rule('vue').uses.delete('cache-loader')
+    config.module.rule('js').uses.delete('cache-loader')
+    config.module.rule('ts').uses.delete('cache-loader')
+    config.module.rule('tsx').uses.delete('cache-loader')
+  },
 }
